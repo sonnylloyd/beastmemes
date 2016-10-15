2d95fcd31bce8bfa15c0baebe3c69a5211ef6f44
Sonny Lloyd <git@sonnylloyd.co.uk>
2016-10-15 08:04:53 +0100

    added caching

--- app/main/controllers/post-ctrl.js
+++ app/main/controllers/post-ctrl.js
@@ -7,35 +7,57 @@ angular.module('main')
     $scope.loadMoreData=function()
     {
 		$http.get('http://beastmemes.com/api/get_posts/?page='+ $scope.page,
-		{header : {'Content-Type' : 'application/json; charset=UTF-8'}})
+		{header : {'Content-Type' : 'application/json; charset=UTF-8'}, timeout: 5000})
 		.success(function(res){
 			var items = res.posts;
 			for (var i = 0; i < items.length; i++) {
 				$scope.items.push(items[i]);
-			} 
+			}
 			if(res.count == 0){
 				$scope.moredata=true;
+			}else{
+				if($scope.page == 1){window.localStorage.setItem('posts', JSON.stringify(res));}
 			}
 			$scope.page = $scope.page + 1;
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		}).error(function (response){
+			if($scope.page == 1 && window.localStorage.getItem('posts')){
+				var res = JSON.parse( window.localStorage.getItem('posts'));
+				var items = res.posts;
+				for (var i = 0; i < items.length; i++) {
+					$scope.items.push(items[i]);
+				}
+				$scope.page = $scope.page + 1;
+			}
+			$scope.$broadcast('scroll.infiniteScrollComplete');
 			$log.log('error' + response);
 		});
     };
 	$scope.doRefresh =function(){
-	$scope.page = 1; 
+	$scope.page = 1;
 		$http.get('http://beastmemes.com/api/get_posts/?page='+ $scope.page,
-			{header : {'Content-Type' : 'application/json; charset=UTF-8'}})
+			{header : {'Content-Type' : 'application/json; charset=UTF-8'}, timeout: 5000})
 			.success(function(res){
 				var items = res.data.posts;
 				for (var i = 0; i < items.length; i++) {
 					$scope.items.push(items[i]);
-				} 
+				}
 				if(res.data.count == 0){
 					$scope.moredata=true;
+				}else{
+					if($scope.page == 1){window.localStorage.setItem('posts', JSON.stringify(res));}
 				}
 				$scope.page = $scope.page + 1;
 		}).error(function (response){
+			if($scope.page == 1 && window.localStorage.getItem('posts')){
+				var res = JSON.parse( window.localStorage.getItem('posts'));
+				var items = res.posts;
+				for (var i = 0; i < items.length; i++) {
+					$scope.items.push(items[i]);
+				}
+				$scope.page = $scope.page + 1;
+			}
+			$scope.$broadcast('scroll.infiniteScrollComplete');
 			$log.log('error' + response);
 		}).finally(function() {
 			$scope.$broadcast('scroll.refreshComplete');
