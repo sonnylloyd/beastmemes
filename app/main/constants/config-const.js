33f239ced5d9bdd02d9867af42ac98afdaaa2685
Sonny Lloyd <git@sonnylloyd.co.uk>
2016-10-23 00:24:08 +0100

    added all urls to constants

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -6,8 +6,15 @@ angular.module('main')
   ENV: {
     /*inject-env*/
     'RECENT_URL': 'http://beastmemes.com/api/get_recent_posts/',
+    'POST_URL': 'http://beastmemes.com/api/get_post/',
     'CATEGORIES_URL': 'http://beastmemes.com/api/get_category_index/',
-    'TAGS_URL': 'http://beastmemes.com/api/get_tag_index/'
+    'CATEGORY_URL': 'http://beastmemes.com/api/get_category_posts/',
+    'TAGS_URL': 'http://beastmemes.com/api/get_tag_index/',
+    'TAG_URL': 'http://beastmemes.com/api/get_tag_posts/',
+    'DATES_URL': 'http://beastmemes.com/api/get_date_index/',
+    'AUTHORS_URL': 'http://beastmemes.com/api/get_author_index/',
+    'AUTHOR_URL': 'http://beastmemes.com/api/get_author_posts/',
+    'SEARCH_URL': 'http://beastmemes.com/api/get_search_results/'
     /*endinject*/
   },
 
